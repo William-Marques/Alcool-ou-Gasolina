0f5b33de62e7d454c89f779581950d4e8d52ec8e
William Marques <matrix.consultoria@gmail.com>
2020-06-10 19:04:44 -0300

    Add Dark Theme

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.java
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.ThemeOverlay_AppCompat_Dark);
         setContentView(R.layout.activity_main);
 
         //linkar as variaveis aos itens da tela
